3a7a56206c357b0e941244f6e3d183ab0b34619f
tieran02 <tieran.wightman@gmail.com>
2020-10-01 13:56:46 +0100

    Newtonian Trajectory

---
+++ Assets/Scripts/NewtonianTrajectory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewtonianTrajectory : MonoBehaviour
+{
+    public Vector3 InitialVelocity;
+
+    private Vector3 StartPosition;
+
+    private const float GRAVITY = -9.8f;
+    private const float TIME_STEP = 1 / 60.0f;
+    private float nextUpdateTime = 0.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the next update is reached
+        if (Time.time >= nextUpdateTime)
+        {
+
+            physicsStep(Time.time);
+
+            nextUpdateTime += TIME_STEP; 
+        }
+    }
+
+    void physicsStep(float t)
+    {
+        float xt = StartPosition.x + (InitialVelocity.x * t);
+        float yt = StartPosition.y + (InitialVelocity.y * t) + (GRAVITY * (t * t)) / 2.0f;
+        float zt = StartPosition.z + (InitialVelocity.z * t);
+
+        transform.position = new Vector3(xt,yt,zt);
+    }
+}
